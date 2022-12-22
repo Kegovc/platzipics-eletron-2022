0b94463056c46fba0a1800a546378a9fbb4f1af6
kegovc <octaviovcruz@hotmail.com>
2022-12-22 00:49:10 -0600

    Generar listas de imagenes

--- src/app.js
+++ src/app.js
@@ -2,8 +2,13 @@
 
 // El objeto app permitirá controlar eventos
 
-const { app, BrowserWindow, ipcMain, dialog } = require ("electron");
-const devtool =  require("./devtools");
+const { app, BrowserWindow, ipcMain, dialog } = require("electron");
+const devtool = require("./devtools");
+
+const fs = require("fs");
+const isImage = require("is-image");
+import path from "path";
+import {filesize} from "filesize";
 
 // console.dir(app)
 let win;
@@ -31,11 +36,11 @@ function createWindow() {
     const position = win.getPosition();
     console.log(`la posición es ${position}`);
   });
-  
-  win.on('closed', () => {
-    win = null
-    app.quit()
-})
+
+  win.on("closed", () => {
+    win = null;
+    app.quit();
+  });
 
   win.loadFile("./renderer/index.html");
 }
@@ -54,7 +59,6 @@ app.whenReady().then(() => {
       createWindow();
     }
   });
-
 });
 
 app.on("window-all-closed", () => {
@@ -63,16 +67,42 @@ app.on("window-all-closed", () => {
   }
 });
 
-ipcMain.on('open-directory',(event)=>{
-  console.log('open-directory')
-  dialog.showOpenDialog(win,{
-    title: 'Selecione la nueva ubicación',
-    buttonLabel: 'Abrir ubicación',
-    properties: ['openDirectory']
-  }).then(dir=>{
-    console.log(dir)
-  })
+ipcMain.on("open-directory", (event) => {
+  console.log("open-directory");
+  dialog
+    .showOpenDialog(win, {
+      title: "Selecione la nueva ubicación",
+      buttonLabel: "Abrir ubicación",
+      properties: ["openDirectory"],
+    })
+    .then((dir) => {
+      if (dir) {
+        const images = [];
+        fs.readdir(dir.filePaths[0], (err, files) => {
+          if (err) {
+            console.log("err", err);
+            throw err
+          }
+
+          images.push(
+            ...files
+              .filter((file) => isImage(file))
+              .map((img) => {
+                const imageFile = path.join(dir.filePaths[0], img)
+                const stats = fs.statSync(imageFile)
+                return {
+                  filename: img,
+                  src: `file://${imageFile}`,
+                  size: filesize(stats.size, {round: 0}),
+                };
+              })
+          );
+          event.sender.send('load-images', images)
+        });
+      }
+      console.log(dir);
+    });
   //event.sender.send('pong', new Date())
-})
+});
 
 // app.quit()
--- src/renderer/ipcRendererEvent.js
+++ src/renderer/ipcRendererEvent.js
@@ -1,16 +1,72 @@
-const {ipcRenderer} = window.require('electron')
+const { ipcRenderer } = window.require("electron");
 
-function setIpc(){
-    ipcRenderer.on('pong', (event,arg)=>{
-        console.log(`pong recibido - ${arg}`)
-    })
+function clearImages() {
+  const oldImages = [...document.querySelectorAll("li.list-group-item")];
+  oldImages.forEach((oldImage) => {
+    oldImage.parentNode.removeChild(oldImage);
+  });
 }
 
-function openDirectory(){
-    ipcRenderer.send('open-directory')
+function loadImages(images) {
+  const imagesList = document.querySelector("ul.list-group");
+  images.forEach((img) => {
+    const node = `<li class="list-group-item">
+    <img
+      class="media-object pull-left"
+      src="${img.src}"
+      height="32"
+    />
+    <div class="media-body">
+      <strong>${img.filename}</strong>
+      <p>${img.size}</p>
+    </div>
+  </li>`;
+    imagesList.insertAdjacentHTML("beforeend", node);
+  });
+}
+
+function addImagesEvents() {
+  const thumbs = [...document.querySelectorAll("li.list-group-item")];
+  thumbs.forEach((thumb) => {
+    thumb.addEventListener("click", function () {
+      changeImage(this);
+    });
+  });
+}
+
+function changeImage(node) {
+  if (!node) {
+    document.getElementById("image-displayed").src = "";
+    return;
+  }
+  document
+    .querySelector("li.list-group-item.selected")
+    ?.classList?.remove("selected");
+  node.classList.add("selected");
+  document.getElementById("image-displayed").src =
+    node.querySelector("img").src;
+}
+
+function selectFirstImage() {
+  const image = document.querySelector("li.list-group-item:not(.hidden)");
+  changeImage(image);
+}
+
+function setIpc() {
+  ipcRenderer.on("load-images", (event, images) => {
+    clearImages();
+    console.log(`load-images recibido - `, images);
+    loadImages(images);
+    addImagesEvents();
+    selectFirstImage()
+  });
+}
+
+function openDirectory() {
+  ipcRenderer.send("open-directory");
 }
 
 module.exports = {
-    setIpc,
-    openDirectory,
-}
+  setIpc,
+  openDirectory,
+};
