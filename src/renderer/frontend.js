63af12e554619309846bd134c4c20a4a9e348313
kegovc <octaviovcruz@hotmail.com>
2022-12-21 20:04:28 -0600

    Selecciona imágenes

--- src/renderer/frontend.js
+++ src/renderer/frontend.js
@@ -1,5 +1,22 @@
-const os = window.require('os')
+// const os = window.require('os')
 
 window.addEventListener("load", () => {
-  console.log(os.cpus());
+  addImagesEvents()
 });
+
+
+function addImagesEvents () {
+  const thumbs = [...document.querySelectorAll('li.list-group-item')]
+  thumbs.forEach(thumb=>{
+    thumb.addEventListener('click',function(){
+      changeImage(this)
+    })
+  })
+}
+
+
+function changeImage(node){
+  document.querySelector('li.list-group-item.selected')?.classList?.remove('selected')
+  node.classList.add('selected')
+  document.getElementById('image-displayed').src = node.querySelector('img').src
+}
